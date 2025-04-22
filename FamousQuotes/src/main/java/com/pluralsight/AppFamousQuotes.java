5504c197c1f382bb78306de6580f6c0bf0340963
Steven Vargas <stevenvari13@gmail.com>
2025-04-22 11:57:16 -0400

    Famous Quotes

---
+++ FamousQuotes/src/main/java/com/pluralsight/AppFamousQuotes.java
@@ -0,0 +1,23 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class AppFamousQuotes {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] famousQuotes = {"The purpose of our lives is to be happy",
+                "Life is a series of lessons which must be lived to be understood.",
+                "Life is like a breathtaking ride on a bicycle, full of challenges and unpredictable curves.",
+                "The only impossible journey is the one you never begin.","The best way to predict the future is to create it.",
+                "Success consists of going from failure to failure without loss of enthusiasm.",
+                "Every strike brings me closer to the next home run.","Life has no limitations, except the ones you make.",
+                "Love the life you live, live the life you love.","Life has no limitations, except the ones you make."};
+
+        System.out.println("Enter a number between 1 - 10 for choose a quote : ");
+        int number = scanner.nextInt();
+        System.out.println("your quote is: " + famousQuotes[number -1]);
+
+
+    }
+}
